f8bc76ed66a94af1d46c5c220bfd8d4a92216cc9
Rory McDonald <rorymcd98@gmail.com>
2023-03-23 20:07:14 +0000

    stash

--- extension-scripts/content/content.tsx
+++ extension-scripts/content/content.tsx
@@ -1,6 +1,44 @@
-(async () => {
-  const src = chrome.extension.getURL('./extension-scripts/content/content_main.js');
-  const contentScript = await import(src);
-  contentScript.main(/* chrome: no need to pass it */);
-})();
+import React from "react";
+import { createRoot } from 'react-dom/client';
+
+//---Initiate content-background script communication channel---
+// content-script.js
+
+let myPort = chrome.runtime.connect({name:"port-from-cs"});
+myPort.postMessage({greeting: "hello from content script"});
+
+myPort.onMessage.addListener((m) => {
+  console.log("In content script, received message from background script: ");
+  console.log(m.greeting);
+});
+
+document.body.addEventListener("click", () => {
+  myPort.postMessage({greeting: "they clicked the page!"});
+});
+
+//---Add 'request-flights-button' buttons to the page---
+
+
+
+// const reactComponent = React.createElement('div');
+
+function ReactComponent() {
+  return (
+    <div id="MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM">vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv</div>
+  )
+}
+
+const domNode = document.getElementsByClassName('c1xntsbd')[0]!;
+
+const root = createRoot(domNode);
+root.render(<ReactComponent></ReactComponent>);
+
+
+//---Gather and send information on 'request-flights-button' button click---
+
+
+//---Receive information from the background script---
+
+
+//---Display flight information on the page---
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,46 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import { crx } from '@crxjs/vite-plugin'
+import manifest from './manifest.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({ fastRefresh: false })],
+  plugins: [react(
+    { fastRefresh: false }),
+    crx({manifest})
+  ],
   worker: {
-    plugins: [react()]
+    plugins: [
+      react()
+    ]
   },
-  root: 'extension-frontend',
+  root: './',
   build: {
     emptyOutDir: true,
     target: 'chrome88',
     assetsDir: 'extension-frontend/src/assets',
     manifest: false,
-    rollupOptions: {
-      input: {
-        popup: resolve(__dirname, 'extension-frontend/src/popup/popup.tsx'),
-        options: resolve(__dirname, 'extension-frontend/src/options/options.tsx')
-      },
-      output: {
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-        assetFileNames: '[name][extname]',
-        format: 'es',
-        dir: resolve(__dirname, 'dist')
-      }
-    }
+   
   },
   server: {
     open: 'public/popup.html'
   }
 })
+
+
+const old = {
+  rollupOptions: {
+    input: {
+      popup: resolve(__dirname, 'extension-frontend/src/popup/popup.tsx'),
+      options: resolve(__dirname, 'extension-frontend/src/options/options.tsx')
+    },
+    output: {
+      entryFileNames: '[name].js',
+      chunkFileNames: '[name].js',
+      assetFileNames: '[name][extname]',
+      format: 'es',
+      dir: resolve(__dirname, 'dist')
+    }
+  }
+}
